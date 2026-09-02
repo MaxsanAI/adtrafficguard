5cfefc957d3124cb975bd527f36ad9f9d7434bde
MaxsanAI <maxsansamurai@gmail.com>
2026-09-02 03:59:21 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,12 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
-export default defineConfig({ output: 'server', adapter: cloudflare(), security: { checkOrigin: true } });
+import vercel from '@astrojs/vercel/serverless';
+
+// Proveravamo koja platforma radi build na osnovu env varijabli
+const adapterToUse = process.env.VERCEL ? vercel() : cloudflare();
+
+export default defineConfig({
+  output: 'server',
+  adapter: adapterToUse,
+  security: { checkOrigin: true }
+});
